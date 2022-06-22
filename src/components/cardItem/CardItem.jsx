f6b72316737cab62ac93ff75e5497d6e9e4f06fa
Irving78rus <isakov911@mail.ru>
2022-06-23 01:30:56 +0300

    first commit

--- src/components/cardItem/CardItem.jsx
+++ src/components/cardItem/CardItem.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
-import './cardItem.css';
-import { useState } from 'react';
-const CardItem = ({ photo, category, name, active, onClick, filterCategory }) => {
-
-  const [press, setPress] = useState(active)
+import React from "react";
+import "./cardItem.css";
+import { useState } from "react";
+const CardItem = ({
+  photo,
+  category,
+  name,
+  active,
+  onClick,
+  filterCategory,
+}) => {
+  const [press, setPress] = useState(active);
 
   const click = () => {
-    onClick()
-    setPress(!press)
-  }
+    onClick();
+    setPress(!press);
+  };
   return (
-    <div className={press ? 'card card__wrapper card__active' : 'card card__wrapper'} onClick={click} >
-      <img src={photo} alt='anyPhoto' />
-      <div className='card__category' onClick={() => { filterCategory(category) }}>{category}</div>
-      <div className='card__name'>{name}</div></div>
+    <div
+      className={
+        press ? "card card__wrapper card__active" : "card card__wrapper"
+      }
+      onClick={click}
+    >
+      <img src={photo} alt="anyPhoto" />
+      <div
+        className="card__category"
+        onClick={() => {
+          filterCategory(category);
+        }}
+      >
+        {category}
+      </div>
+      <div className="card__name">{name}</div>
+    </div>
   );
-}
+};
 
 export default CardItem;
