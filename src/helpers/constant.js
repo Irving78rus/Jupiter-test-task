be8c39378b9666a001f7b70ecbc1ea12acdbc8c7
Irving78rus <isakov911@mail.ru>
2022-06-24 01:34:18 +0300

    Updates

--- src/helpers/constant.js
+++
@@ -1,9 +0,0 @@
-export const  filterOptions ={
-    SHOW_ALL: 'Show All',
-    DESIGN:"Design",
-    BRANDING:"Branding",
-    ILLUSTRATION:"Illustration",
-    MOTION:"Motion"
-}
-export const cardsLimit = 9;
- 
